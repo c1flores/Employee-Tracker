3d5bc62806753ab92643f28b587a4fd4402fc26e
Christian Flores <c1flores@ucsd.edu>
2022-10-26 11:35:01 -0700

    Create constructor methods for SQL database

---
+++ db/index.js
@@ -0,0 +1,61 @@
+const connection = require("./connection");
+
+class EmployeeDB {
+
+    constructor(connection) {
+        this.connection = connection;
+    }
+
+    // Method to view all employees
+    allEmployees(){
+        return this.connection.promise().query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;" 
+        );
+    }
+
+    // Method to add an employee
+    addEmployee(employee) {
+        return this.connection.promise().query("INSERT INTO employee SET ?", employee);
+    }
+
+    // Method to update the employee's role
+    updateEmployeeRole(employeeID, roleId) {
+        return this.connection.promise().query(
+            "UPDATE employee SET role_id = ? WHERE id = ?",
+            [roleId, employeeId]
+        );
+    }
+
+    // Method to show all managers
+    allManagers(employeeId) {
+        return this.connection.promise().query(
+            "SELECT id, first_name, last_name FROM employee WHERE id != ?",
+            employeeId
+        );
+    }
+
+    // Method to show all roles
+    allRoles() {
+        return this.connection.promise().query(
+            "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+        );
+    }
+
+    // Method to add a role
+    addRole(role) {
+        return this.connection.promise().query("INSERT INTO role SET ?", role);
+    }
+
+    // Method to show all departments
+    allDepartments() {
+        return this.connection.promise().query("SELECT department.id, department.name FROM department;"
+        );
+    }
+
+    // Method to add a department 
+    addDepartment(department) {
+        return this.connection.promise().query("INSERT INTO role SET ?", department);
+    }
+}
+
+module.exports = new employeeDB(connection);
